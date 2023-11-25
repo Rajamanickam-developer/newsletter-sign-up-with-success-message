b4a2d714a678e96214b731de28c77ee94b0009e1
Rajamanickam <rajamanickam842001@gmail.com>
2023-11-25 14:28:27 +0530

    final commit

--- js/validation.js
+++ js/validation.js
@@ -1,6 +1,34 @@
+const form = document.querySelector('.input-container');
+const newsletter = document.querySelector('.newsletter-container');
+const popup = document.querySelector('.popup-container');
+const input = document.querySelector('.email-input');
+const errorEle = document.querySelector('.error-msg');
+const getEmail = document.getElementById('get-email');
+const popupBtn = document.querySelector('popup-btn');
 
-function emailValidation() {
-  let inputElement = document.getElementsByClassName('.email-input');
+form.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  emailValidation();
+})
 
+
+function emailValidation(){
   
+  let emailCheck = !input.value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  
+  if (emailCheck) {
+    errorEle.innerHTML = 'Valid email required';
+    input.classList.add('error');
+    return false;
+  }else{
+    newsletter.classList.add('hide');
+    popup.classList.remove('hide');
+    let val = input.value;
+    getEmail.innerHTML = val;
+  }
+}
+
+
+function dismissMsg(){
+  newsletter.classList.remove('hide');
 }
